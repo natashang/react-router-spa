55a96ebdf51be93754bd27bf8a7960b8b98f07df
natashang <50751197+natashang@users.noreply.github.com>
2020-09-28 16:42:03 -0700

    uncommenting build

---
+++ build/precache-manifest.2c2038c4600edb3c7cd3fa418b90ff38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f4b5888364c2a0fad92b85caa7840c",
+    "url": "/react-router-spa/index.html"
+  },
+  {
+    "revision": "b14e7b19ac6607a02b01",
+    "url": "/react-router-spa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dda4f943649d019bfbf3",
+    "url": "/react-router-spa/static/js/2.806cb436.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router-spa/static/js/2.806cb436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14e7b19ac6607a02b01",
+    "url": "/react-router-spa/static/js/main.c49cf2bd.chunk.js"
+  },
+  {
+    "revision": "3bae369c043eff83b1aa",
+    "url": "/react-router-spa/static/js/runtime-main.570a3411.js"
+  }
+]);
